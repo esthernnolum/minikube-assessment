e02e8bb79f5a12bc1d3676b3b1c9fee57e426279
Esther Nnolum <ennolum@MACs-MacBook-Pro.local>
2023-08-19 07:18:15 +0100

    update app.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# Define your favorite colour
+favorite_colour = "SkyBlue"
+
 @app.route('/foo', methods=['GET'])
-def get_favourite_color():
-    response = {"myFavouriteColor": "SkyBlue"}
-    return jsonify(response)
+def get_favorite_colour():
+    response_data = {"myFavouriteColour": favorite_colour}
+    return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(port=8080)
